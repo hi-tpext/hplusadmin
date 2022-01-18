a92579e68763f6fdfa7d3f4f69b50dbf31201fa8
ichynul <ichynul@163.com>
2022-01-18 16:08:02 +0800

    fix:开启顶部菜单后，左侧多级菜单折叠问题

--- common/Resource.php
+++ common/Resource.php
@@ -7,7 +7,7 @@ use tpext\myadmin\common\Module as adminModule;
 
 class Resource extends baseResource
 {
-    protected $version = '1.0.5';
+    protected $version = '1.0.6';
 
     protected $name = 'hplus.admin';
 
