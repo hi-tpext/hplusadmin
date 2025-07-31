34e06628d92282cf9fff1792ed793d492197cb0d
ichynul <ichynul@163.com>
2025-07-31 19:04:01 +0800

    调整

--- common/Resource.php
+++ common/Resource.php
@@ -7,7 +7,7 @@ use tpext\myadmin\common\Module as adminModule;
 
 class Resource extends baseResource
 {
-    protected $version = '1.0.8';
+    protected $version = '1.0.9';
 
     protected $name = 'hplus.admin';
 
